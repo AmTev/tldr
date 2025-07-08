077adced1d7afbe8784c424a1cc79437d537bff3
Ram Shrestha <ramshrestha.developer@gmail.com>
2025-07-08 20:27:32 +0545

    update video docs

--- inc/class.admin.tldr.php
+++ inc/class.admin.tldr.php
@@ -178,7 +178,7 @@ class TLDR_Plugin
           </div>
 
           <div class="tldr-docs-video" style="flex: 1; max-width: 560px;">
-            <div style="padding:56.25% 0 0 0;position:relative;">
+            <div style="padding:56.25% 0 20px 0;position:relative;">
               <iframe src="https://player.vimeo.com/video/1099170110?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
                 frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share"
                 style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TL;DR"></iframe>
